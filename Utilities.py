86589ff1154e2f6bd86ed2bcbc4f152bfcfff6f3
Breandan <kerinb@tcd.ie>
2017-12-04 21:58:25 +0000

    added skeleton method in utilities that will clone from a given repo

---
+++ Utilities.py
@@ -0,0 +1,5 @@
+CLONED_GIT_REPOSITORY = "https://bitbucket.org/Breandan96/cs4400distributedfileserver/commits/all"
+
+
+def clone_git_repo(url):
+    print 'in git clone repo function in utilities\n Requested to clone{}'.format(url)
